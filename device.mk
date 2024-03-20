e5d9a1cd415053f34c83b279d6d6893c4c5dcc1f
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-20 17:21:34 +0530

    kunzite: Build libcamera2ndk_vendor from source
    Change-Id: I23440f4437ec704829054a1680b5ac812fb76006

--- device.mk
+++ device.mk
@@ -116,6 +116,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti.recovery
 
 # Camera
+PRODUCT_PACKAGES += \
+    libcamera2ndk_vendor
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
