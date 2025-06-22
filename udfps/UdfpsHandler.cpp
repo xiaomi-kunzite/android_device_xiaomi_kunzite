d98049713ee0b2ae36d912cb48d882e8d5213a7e
Giovanni Ricca <giovanniricca@proton.me>
2025-06-22 11:10:29 +0200

    kunzite: udfps: Workaround `onFingerDown()` race
    `onFingerDown()` may be called after authentication has been completed,
    leaving HBM enabled. Use a dummy variable to skip HBM activation when
    authentication has already succeeded.
    
    Note: This is a temporary solution until we figure out what is really
    going on.
    
    Change-Id: Ie738b0dbe8a51e99f2bf9f922b60e725fa2888ee

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -175,6 +175,7 @@ class XiaomiKunziteUdfpsHandler : public UdfpsHandler {
     }
 
     void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        if (mAuthSuccess) return;
         LOG(INFO) << __func__;
         
         // Notify HAL about finger press
@@ -255,7 +256,14 @@ class XiaomiKunziteUdfpsHandler : public UdfpsHandler {
         ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &displayLhbmRequest);
     }
 
-    void onAuthenticationSucceeded() { onFingerUp(); }
+    void onAuthenticationSucceeded() {
+        mAuthSuccess = true;
+        onFingerUp();
+        std::thread([this]() {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            mAuthSuccess = false;
+        }).detach();
+    }
 
     void onAuthenticationFailed() { onFingerUp(); }
 
@@ -263,6 +271,7 @@ class XiaomiKunziteUdfpsHandler : public UdfpsHandler {
     fingerprint_device_t* mDevice;
     android::base::unique_fd touch_fd_;
     android::base::unique_fd disp_fd_;
+    bool mAuthSuccess = false;
 
     void setFodStatus(int value) {
         ioctl(touch_fd_.get(), TOUCH_IOC_SELECT_TOUCH_ID, MI_DISP_PRIMARY);
