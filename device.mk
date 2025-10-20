3c690c072dd732462082fb54a0c1d45e4605df0a
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-20 22:20:55 +0530

    kunzite: Build QTI USB service
    Change-Id: Ifb20247742bd6ecabf8e7cb42d065040ed522071

--- device.mk
+++ device.mk
@@ -297,6 +297,18 @@ PRODUCT_PACKAGES += \
     update_verifier
 
 # USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
