10c890fe3b335974d779b10908512ce9b204c12c
Byben <heybyben@gmail.com>
2026-06-05 13:51:05 +0700

    kunzite: Setup kernel args
    Change-Id: I08b3bdd8c56edb1f5aa74d17faaf497f8ce2e84b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,38 @@ TARGET_CPU_VARIANT_RUNTIME := kryo300
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := parrot
 
+# Kernel
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_RAMDISK_USE_LZ4 := true
+TARGET_NEEDS_DTBOIMAGE := true
+
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_IMAGE_NAME := Image
+
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+BOARD_INIT_BOOT_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
+
+BOARD_KERNEL_CMDLINE := \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    swinfo.fingerprint=$(LINEAGE_VERSION) \
+    mtdoops.fingerprint=$(LINEAGE_VERSION) \
+    bootmonitor.fingerprint=$(LINEAGE_VERSION)
+
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.load_modules_parallel=true \
+    androidboot.hypervisor.protected_vm.supported=true \
+    androidboot.vendor.qspa=true
+
+BOARD_BOOTCONFIG += \
+    androidboot.selinux=permissive
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_DTBOIMG_PARTITION_SIZE := 24117248
