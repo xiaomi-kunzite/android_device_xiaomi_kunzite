0131786a00d308ad632305dc2ebbfa09478715b3
heybyben <heybyben@gmail.com>
2026-06-05 18:12:11 +0000

    kunzite: Import overlays
    Change-Id: Ib81ecdbce1e9f20d88b75932ca53b82a8ada3c35

--- device.mk
+++ device.mk
@@ -105,6 +105,17 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayKunzite \
+    FrameworksOverlayKunzite \
+    NfcOverlayKunzite \
+    SecureElementOverlayKunzite \
+    SettingsOverlayKunzite \
+    SystemUIOverlayKunzite \
+    TelephonyOverlayKunzite \
+    WifiOverlayKunzite
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
