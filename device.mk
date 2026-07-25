dcd653ee8f177c7b467bfb86adf9efdec56f8f58
heybyben <heybyben@gmail.com>
2026-07-25 18:31:47 +0000

    kunzite: Build common AIDL IR service * and copy permission from aosp
    Change-Id: Ieaf4a45802039ca21a990ffec6ed659929fac85f

--- device.mk
+++ device.mk
@@ -196,6 +196,13 @@ PRODUCT_PACKAGES += \
     ueventd-odm.rc \
     ueventd-qcom.rc
 
+# IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir-service.lineage
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # Keymint
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
